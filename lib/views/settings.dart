c028d4cd741be2d735e28a6da9dc1a11cd0d062a
neolanco <www.leon098@gmail.com>
2024-10-14 01:54:26 +0200

    settings view: add source button

--- lib/views/settings.dart
+++ lib/views/settings.dart
@@ -1,5 +1,6 @@
 // views/settings.dart
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key});
@@ -10,9 +11,44 @@ class SettingsPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Settings'),
       ),
-      body: const Center(
-        child: Text('Settings will go here'),
+      body: Center(
+        child: ListView(
+          children: const <Widget>[cardSource()],
+        ),
       ),
     );
+  }
+}
+
+class cardSource extends StatelessWidget {
+  const cardSource({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 10,
+      child: InkWell(
+        onTap: () {
+          debugPrint('cardSource tapped');
+          launcherSource();
+        },
+        child: const SizedBox(
+          child: ListTile(
+            leading: Icon(Icons.code),
+            title: Text("Source"),
+            subtitle: Text("Get Updates & More!"),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+launcherSource() async {
+  final Uri url = Uri.parse('https://github.com/Neolanco/ARSvp-Flutter');
+  if (!await launchUrl(url)) {
+    throw Exception('Could not launch $url');
   }
 }
